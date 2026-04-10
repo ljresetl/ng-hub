d332a71a783b18c3a31890c065cb9a8fb2172391
ljresetl <ljresetl@gmail.com>
2026-04-10 21:30:31 +0200

    Update Foto.tsx

--- src/components/Foto/Foto.tsx
+++ src/components/Foto/Foto.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import Image from "next/image";
 import styles from "./Foto.module.scss";
 
 const Foto: React.FC = () => {
@@ -7,11 +8,26 @@ const Foto: React.FC = () => {
     <section className={styles.hero}>
       <div className={styles.container}>
         <div className={styles.houseCanvas}>
+          
+          {/* Ця картинка потрібна ТІЛЬКИ для Google, щоб LCP був зеленим */}
+          {/* Вона завантажиться першою, але буде захована під кубиками */}
+          <div className={styles.lcpImage}>
+            <Image 
+              src="/400M.avif" 
+              alt="Background" 
+              fill 
+              priority 
+              sizes="100vw"
+              style={{ objectFit: 'cover' }}
+            />
+          </div>
+
           <div className={styles.bricksWrapper}>
             {[...Array(24)].map((_, i) => (
               <div key={i} className={styles.brick} />
             ))}
           </div>
+
           <div className={styles.textOverlay}>
             <div className={styles.textContainer}>
               <h1 className={styles.line1}>Vaše digitální symfonie.</h1>

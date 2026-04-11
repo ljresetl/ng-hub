af3707914513b760b22c97e4d0b586af509fcbce
ljresetl <ljresetl@gmail.com>
2026-04-12 01:32:57 +0200

    Update Foto.tsx

--- src/components/Foto/Foto.tsx
+++ src/components/Foto/Foto.tsx
@@ -10,35 +10,18 @@ const Foto: React.FC = () => {
       <div className={styles.container}>
         <div className={styles.houseCanvas}>
           
-          {/* МОБІЛЬНЕ ФОТО: пріоритет №1 */}
-          <div className={styles.mobileOnly}>
-            <Image 
-              src="/1775925937764.avif"
-              alt="NG Consulting Mobile"
-              fill
-              priority // Next.js додасть preload
-              // @ts-ignore - щоб TS не сварився на специфічний атрибут
-              fetchPriority="high" // ПРЯМА вказівка для PageSpeed
-              quality={85}
-              sizes="(max-width: 767px) 100vw, 1px"
-              className={styles.bgImage}
-            />
-          </div>
-
-          {/* ДЕСКТОПНЕ ФОТО */}
-          <div className={styles.desktopOnly}>
-            <Image 
-              src="/1775925937764.avif" 
-              alt="NG Consulting Desktop"
-              fill
-              priority
-              // @ts-ignore
-              fetchPriority="high"
-              quality={90}
-              sizes="(min-width: 768px) 100vw, 1px"
-              className={styles.bgImage}
-            />
-          </div>
+          {/* ТЕПЕР ТУТ ТІЛЬКИ ОДИН ТЕГ IMAGE */}
+          <Image 
+            src="/1775925937764.avif"
+            alt="NG Consulting - Digitální symfonie"
+            fill
+            priority 
+            // @ts-ignore
+            fetchPriority="high" 
+            quality={85}
+            sizes="100vw" 
+            className={styles.bgImage}
+          />
 
           <div className={styles.textOverlay}>
             <div className={styles.textContainer}>
@@ -59,3 +42,4 @@ const Foto: React.FC = () => {
 };
 
 export default Foto;
+
